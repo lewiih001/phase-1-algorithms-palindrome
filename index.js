2185b0178ba4d43a62daf613a82821f834cd99c9
Lewis <lewismunyambu@gmail.com>
2022-09-06 14:11:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-function isPalindrome(str) {
-  var len = str.length;
+function isPalindrome(word) {
+  var len = word.length;
   var mid = Math.floor(len/2);
 
   for ( var i = 0; i < mid; i++ ) {
-      if (str[i] !== str[len - 1 - i]) {
+      if (word[i] !== word[len - 1 - i]) {
           return false;
       }
   }
-
   return true;
-
 }
  
 /* 
